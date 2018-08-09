88925728dcd5b43cea2bbbed97d730aab9d63c32
wyminc <wyminc808@gmail.com>
2018-08-08 20:31:28 -1000

    cleaned client.js folder to not show console.logs

--- client.js
+++ client.js
@@ -1,8 +1,6 @@
 const net = require("net");
 
 const client = net.createConnection(2877, "34.219.80.229", () => {
-  console.log(client, "client");
-  console.log(client.localAddress);
   client.on("data", data => {
     console.log(data.toString());
   });
